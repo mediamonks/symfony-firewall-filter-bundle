61555c7242eaa6f9d1c85a69c6033472c00f4579
Pawel <pawel@mediamonks.com>
2016-07-06 15:46:38 +0200

    Strict typing for check

--- Security/LoginFlow/CheckAwareInterface.php
+++ Security/LoginFlow/CheckAwareInterface.php
@@ -2,6 +2,8 @@
 
 namespace MediaMonks\FirewallFilterBundle\Security\LoginFlow;
 
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
 /**
  * Interface CheckAwareInterface
  * @package MediaMonks\FirewallFilterBundle\Security\LoginFlow
@@ -9,5 +11,5 @@ namespace MediaMonks\FirewallFilterBundle\Security\LoginFlow;
  */
 interface CheckAwareInterface
 {
-    public function onCheck($event);
+    public function onCheck(GetResponseEvent $event);
 }
