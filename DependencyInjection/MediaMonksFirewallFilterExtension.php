60b9f9e095d94fda13ca86b7bf38e950e37f9203
Pawel <pawel@mediamonks.com>
2016-07-07 13:46:24 +0200

    travis + scrutinizer test, unused use statement

--- DependencyInjection/MediaMonksFirewallFilterExtension.php
+++ DependencyInjection/MediaMonksFirewallFilterExtension.php
@@ -2,7 +2,6 @@
 
 namespace MediaMonks\FirewallFilterBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
